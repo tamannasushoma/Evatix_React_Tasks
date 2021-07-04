e1756e6dc76f02d600af02d0cef688acd4112760
tamanna islam sushoma <tamannais18@gmail.com>
2021-07-05 04:57:52 +0600

    changes

--- practical-tasks/src/Components/Card-holder/card-container.js
+++ practical-tasks/src/Components/Card-holder/card-container.js
@@ -7,15 +7,18 @@ class Cards extends Component {
     }
     render() { 
         return ( 
-            <div>
-            <div className="flex flex-wrap object-cover inline-flex mx-auto mt-8 ">
-
+           <div>
+                <div className="">
+                    <h1 className=" text-xl lg:text-3xl font-bold pt-8 lg:pt-20 text-blue-500">Packages</h1>
+                    <hr className="bg-blue-500 w-1/5 m-auto h-1 mt-2"/>
+                
+            <div className="flex flex-wrap object-cover inline-flex mx-auto mt-2 ">
             <Card/>
             <Card/>
             <Card/>
             </div>
-
             </div>
+        </div>
          );
     }
 }
